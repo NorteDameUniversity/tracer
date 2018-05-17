ae13bd4786e593e80fb198c9d3fa99c66991df8b
elvena <37617232+elvena@users.noreply.github.com>
2018-05-17 10:12:08 +0800

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('tracerApp')
-  .controller('MainController', ['$http', function($http) {
-    var ctrl = this;
-
-    ctrl.isUserAuthenticated = window.DJANGO.isUserAuthenticated;
-
-  }]);
