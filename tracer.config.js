d77ca5ac279f5d2fbf2acb4d82f7af11a9a5161a
elvena <37617232+elvena@users.noreply.github.com>
2018-05-17 10:15:37 +0800

    Delete tracer.config.js

--- tracer.config.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('tracerApp')
-  .config(['$httpProvider', function($httpProvider) {
-    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-  }]);
